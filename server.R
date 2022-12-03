cb289e5c5e11faf438d1f1dff9f4f0daa4e2816b
Luiz Avelar <luiz.avelar25@gmail.com>
2022-12-03 14:54:24 -0300

    feat: add table

--- server.R
+++ server.R
@@ -1,21 +1,32 @@
 library(shiny)
 library(tidyverse)
-library(readr)
 library(DT)
-library(stringr)
 library(plotly)
 
-
 base <- read.csv("superliga_202122.csv")
+names(base) <- c("SET_1","SET_2","SET_3","SET_4","SET_5","Jogadora","Time","Partida","Vencedor","Servico_Err","Servico_Ace","Recepcao_Tot","Recepcao_Err","Ataque_Exc","Ataque_Err","Ataque_Blk","Bloqueio_Pts","Fase","Cat","Jogo","VV")
 
 server <- function(input, output, session) {
   
-  output$tabela <- renderDataTable({
-    base|>group_by(Vencedor) |> summarise(Vitorias = n_distinct(Partida)) |> rename(Time = Vencedor) |> arrange(desc(Vitorias))
+  # Aba de times
+  output$tabela1 <- renderDataTable({
+    base|> group_by(Vencedor) |> summarise(Vitorias = n_distinct(Partida)) |> rename(Time = Vencedor) |> arrange(desc(Vitorias))
   })
   
+  output$tabela2 <- renderDataTable({
+    base|> group_by(Time) |> summarise(VV = sum(VV)) |> arrange(desc(VV))
+  })
+  
+  # Aba jogadoras
+  
+  Rec <-  base |> 
+    group_by(Jogadora, Time) %>%
+    summarise(Quantidade_de_Recepções = sum(Recepcao_Tot, na.rm = T), Erros_de_Recepção = sum(Recepcao_Err, na.rm = T)) %>%
+    mutate('Proporção entre Erros e Recepções' = Erros_de_Recepção / Quantidade_de_Recepções) %>%
+    filter(Quantidade_de_Recepções > 100)
+  
   output$plot1 <- renderPlot({
-    ggplot(Rec, mapping = aes(x = Recepcao_Total, y = Erro_Recepcao)) +
+    ggplot(Rec, mapping = aes(x = Quantidade_de_Recepções, y = Erros_de_Recepção)) +
       geom_point()
   })
   
--- ui.R
+++ ui.R
@@ -1,8 +1,5 @@
-vars <- c("Serviço.Err", "Serviço.Ace", "Recepção.Tot", "Recepção.Err", "Ataque.Exc.", "Ataque.Err", "Ataque.Blk", "Bloqueio.Pts")
-
+vars <- c("Servico_Err","Servico_Ace","Recepcao_Tot","Recepcao_Err","Ataque_Exc","Ataque_Err","Ataque_Blk","Bloqueio_Pts")
    
-
-
 ui <- navbarPage(
   
   theme = bslib::bs_theme(primary = "#5F2A90", secondary = "#90E124", success = "#DFB60B", bootswatch = "flatly"),
@@ -10,13 +7,15 @@ ui <- navbarPage(
   "Superliga Brasileira de Voleibol Feminino (21/22)",
   tabPanel("Jogadoras",
            tabsetPanel(
-             tabPanel("Recepcao", plotOutput("plot1", brush = "plot_brush"), tableOutput("table2"))
+             h3("Quantidade de Erros de recepção por recepções tentadas"),
+             h6("Destaque um ponto para informações detalhadas"),
+             tabPanel("Recepção", plotOutput("plot1", brush = "plot_brush"), tableOutput("table2"))
            ),
            
            tabsetPanel(
              sidebarPanel(
                selectInput('ycol','Y Variable', vars),
-               selected = "Serviço.Ace"
+               selected = "Servico_Ace"
             ),
             tabPanel("Stats",
                plotlyOutput('plot')
@@ -24,8 +23,12 @@ ui <- navbarPage(
            )
   ),
   tabPanel("Times",
+           
            h3("Quantidade de vitórias por time"),
-           tabPanel("teste",dataTableOutput("tabela"))
+           tabPanel("tabela1", dataTableOutput("tabela1")),
+           
+           h3("Quantidade de prêmios de melhor jogadora da partida por time"),
+           tabPanel("tabela2", dataTableOutput("tabela2")),
   )
   
 )
